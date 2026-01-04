2c33848e5f84f44f1c462ab6d38ea4d14e20941b
delhassali5620-lang <d.elhassali5620@uca.ac.ma>
2026-01-04 20:13:23 +0100

    Create sequence_range.py

---
+++ sequence_range.py
@@ -0,0 +1,13 @@
+# Étape 3 : for + range
+print("Nombres de 1 à 5 :")
+for i in range(1, 6):
+    print(f"Nombre {i}")
+
+print("Nombres de 0 à 10 par 2 :")
+for i in range(0, 11, 2):
+    print(i)
+
+print("Nombres de 10 à 1 :")
+for i in range(10, 0, -1):
+    print(i)
+
